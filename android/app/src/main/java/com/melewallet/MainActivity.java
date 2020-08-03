0600e78a2553fb3adb4ccffa51efba57c1ad4460
levan basharuli <l.basharuli@gmail.com>
2020-08-03 14:11:05 +0400

    renamed

--- android/app/src/main/java/com/melewallet/MainActivity.java
+++ android/app/src/main/java/com/melewallet/MainActivity.java
@@ -1,4 +1,4 @@
-package com.memewallet;
+package com.melewallet;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "MemeWallet";
+    return "MeleWallet";
   }
 }
--- android/app/src/main/java/com/melewallet/MainApplication.java
+++ android/app/src/main/java/com/melewallet/MainApplication.java
@@ -1,4 +1,4 @@
-package com.memewallet;
+package com.melewallet;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.memewallet.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.melewallet.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
