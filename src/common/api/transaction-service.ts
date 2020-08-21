e33ab12f13771802edc99801298a3ae74885012b
levan basharuli <l.basharuli@gmail.com>
2020-08-21 20:18:05 +0400

    added url builder

--- src/common/api/transaction-service.ts
+++ src/common/api/transaction-service.ts
@@ -1,6 +1,26 @@
 import MainService from "./main-api-service";
-
+import buildUrl from "build-url";
 // used by admin to manipulate user data
+export enum TransactionType {
+	TRANSFER = "transfer",
+	PURCHASE = "purchase",
+}
+
+export enum TransactionStatus {
+	PENDING = "pending",
+	DENIED = "denied",
+	APPROVED = "approved",
+}
+
+export interface ISearchTransactionParameter {
+	page: number;
+	size: number;
+	from?: string;
+	to?: string;
+	type?: TransactionType;
+	status?: TransactionStatus;
+}
+
 export default class TransactionService extends MainService {
 	transactionSend = async (to: string, amount: number) => {
 		return await this.put({
@@ -12,9 +32,16 @@ export default class TransactionService extends MainService {
 			},
 		});
 	};
-	searchTransactions = async (page: number, size: number) => {
+	searchTransactions = async (p: ISearchTransactionParameter) => {
+		const url = buildUrl("/transaction", {
+			queryParams: {
+				from: "1000-01-01",
+				to: "3000-01-01",
+				...(p as any),
+			},
+		});
 		return await this.get({
-			path: `/transaction?size=${size}&to=3000-07-01&from=2019-08-20&page=${page}`,
+			path: url,
 		});
 	};
 }
