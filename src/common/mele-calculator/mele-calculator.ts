fc4f724330e340a963e416f7bd171bda8ab6084e
levan basharuli <l.basharuli@gmail.com>
2020-08-23 17:01:44 +0400

    fixes in calculator

--- src/common/mele-calculator/mele-calculator.ts
+++ src/common/mele-calculator/mele-calculator.ts
@@ -1,9 +1,9 @@
 export class MeleCalculator {
 	public static CentsToMeleC(cents: number, meleCPrice: number) {
-		return (cents * meleCPrice) / 100;
+		return cents / meleCPrice;
 	}
 	public static CentsToMeleG(cents: number, meleGPrice: number) {
-		return Math.round((cents * meleGPrice) / 100);
+		return cents / meleGPrice;
 	}
 
 	public static CentsToMeleCFormatted(
@@ -11,17 +11,47 @@ export class MeleCalculator {
 		meleCPrice: number,
 		decimals: number = 5,
 	) {
-		return parseFloat(
-			MeleCalculator.CentsToMeleC(cents, meleCPrice).toFixed(decimals),
-		).toString() as any;
+		return MeleCalculator.formatNumber(
+			MeleCalculator.CentsToMeleC(cents, meleCPrice),
+			decimals,
+		);
 	}
 	public static CentsToMeleGFormatted(
 		cents: number,
 		meleCPrice: number,
 		decimals: number = 5,
 	) {
-		return parseFloat(
-			MeleCalculator.CentsToMeleG(cents, meleCPrice).toFixed(decimals),
-		).toString() as any;
+		return MeleCalculator.formatNumber(
+			MeleCalculator.CentsToMeleG(cents, meleCPrice),
+			decimals,
+		);
 	}
+
+	public static centsToUSD = (cents: number) => {
+		return cents / 100;
+	};
+
+	public static centsToUSDFormatted = (cents: number, decimals: number = 2) => {
+		return MeleCalculator.USDFormatted(
+			MeleCalculator.centsToUSD(cents),
+			decimals,
+		);
+	};
+	public static USDFormatted = (USD: number, decimals: number = 2) => {
+		return MeleCalculator.formatNumber(USD, decimals);
+	};
+
+	public static formatNumber = (numb: number, decimals: number = 2) => {
+		let [round, decimal] = parseFloat(numb.toFixed(decimals))
+			.toString()
+			.split(".");
+		round = round.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+
+		if (decimal) {
+			return [round.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,"), decimal].join(
+				".",
+			) as any;
+		}
+		return round as any;
+	};
 }
