89a4c75f2d5e9391c6ce493e3dda69490f07c296
levan basharuli <l.basharuli@gmail.com>
2020-08-31 09:23:32 +0400

    added filter to users endpoint

--- src/common/api/user-service.ts
+++ src/common/api/user-service.ts
@@ -1,10 +1,32 @@
 import MainService from "./main-api-service";
+import buildUrl from "build-url";
+
+export interface ISearchUsersParameter {
+	page?: number;
+	size?: number;
+	status?: string;
+}
 
 // used by admin to manipulate user data
 export default class UserService extends MainService {
-	searchUsers = async (page: number, size: number) => {
+	searchUsers = async (p: ISearchUsersParameter) => {
+		// remove this after API uses standard filters
+		const tempP: any = {};
+		if (p.status == "pending") {
+			tempP.pending = "";
+		}
+		// <==== till this
+
+		const url = buildUrl("/accounts", {
+			queryParams: {
+				page: p.page || 1,
+				size: p.size || 20,
+				...(p as any),
+				...tempP,
+			},
+		});
 		return await this.get({
-			path: `/accounts?page=${page || 1}&size=${size || 20}`,
+			path: url,
 		});
 	};
 	loadUserByEmail = async (email: string) => {
