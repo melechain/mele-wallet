986600da9ba212aa4e4966c95d918f9a6e9d6354
Andrej Budinčević <andrew@hotmail.rs>
2021-07-13 10:54:27 +0200

    Add mock implementation for fs in the metro config and include cjs.js extension for the module resolver

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig } = require("metro-config");
+const path = require("path");
 
 module.exports = (async () => {
 	const {
@@ -16,8 +17,11 @@ module.exports = (async () => {
 		},
 		resolver: {
 			assetExts: assetExts.filter((ext) => ext !== "svg"),
-			sourceExts: [...sourceExts, "svg", "umd.js"],
-			extraNodeModules: require("node-libs-react-native"),
+			sourceExts: [...sourceExts, "svg", "umd.js", "cjs.js"],
+			extraNodeModules: {
+				...require("node-libs-react-native"),
+				fs: path.resolve(__dirname, "./node_modules/fake-fs"),
+			},
 		},
 	};
 })();
