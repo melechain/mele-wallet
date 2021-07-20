ffbaeb945fd850c25699cf7483b980372a6a8cc8
DMachado93 <66080295+DMachado93@users.noreply.github.com>
2021-07-20 12:02:40 +0100

    more screen

--- src/app/modules/more/more.tsx
+++ src/app/modules/more/more.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { View, ScrollView, Text, StatusBar } from "react-native";
-import Clipboard from "@react-native-community/clipboard";
 import { connect } from "react-redux";
 import ApplicationState from "@mele-wallet/redux/application-state";
 import {
@@ -10,25 +9,14 @@ import {
 import { AccountState } from "@mele-wallet/redux/reducers/account-reducer";
 import { styles } from "./styles";
 import { commonStyles } from "@mele-wallet/app/common/styles/common-styles";
-import CurrencyIcon from "@mele-wallet/resources/icons/currency.svg";
-import RateAppIcon from "@mele-wallet/resources/icons/rate.svg";
 import WalletIcon from "@mele-wallet/resources/icons/wallet.svg";
-import KYCIcon from "@mele-wallet/resources/icons/kycver.svg";
 import LockGreenIcon from "@mele-wallet/resources/icons/lock-green.svg";
-import InfoIcon from "@mele-wallet/resources/icons/info.svg";
-import ShieldBlueIcon from "@mele-wallet/resources/icons/shield-blue.svg";
-import LanguageIcon from "@mele-wallet/resources/icons/language.svg";
 import Ripple from "react-native-material-ripple";
-import { Calculator } from "@mele-wallet/app/common/calculator/calculator";
-import { Wallet } from "@mele-wallet/common/utils/wallet";
 import { StaticState } from "@mele-wallet/redux/reducers/static-reducer";
-import { Actions, Actions as FluxActions } from "react-native-router-flux";
+import { Actions as FluxActions } from "react-native-router-flux";
 import { ROUTES } from "@mele-wallet/app/router/routes";
 import { LanguageState } from "./../../../redux/reducers/language-reducer";
 import { CurrencyState } from "@mele-wallet/redux/reducers/currency-reducer";
-import { CurrencyPicker } from "@mele-wallet/app/common/pickers/currency-picker";
-import { LanguagePicker } from "@mele-wallet/app/common/pickers/language-picker";
-import { Picker } from "@react-native-picker/picker";
 import { Utils } from "mele-sdk";
 import AsyncStorage from "@react-native-community/async-storage";
 
